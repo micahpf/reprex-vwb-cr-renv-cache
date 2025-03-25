53845e4c15dcc1b7b4fe7f82846db10923592318
Micah Fletcher <micahpfletcher@gmail.com>
2025-03-25 16:52:47 +0000

    Create reprex-vwb-cr-renv-cache.R

---
+++ reprex-vwb-cr-renv-cache.R
@@ -0,0 +1,32 @@
+# First, clone github.com/micahpf/reprex-vwb-cr-renv-cache into ~/repos
+# and open the R Studio Project
+
+## Set up controlled resource bucket and mount it
+# Create controlled resource
+system("wb resource resolve --name renv-cache-2 || wb resource create gcs-bucket --name=renv-cache-2 --cloning=COPY_NOTHING --description='Bucket for caching R package binaries using renv'")
+# Mount new resources
+system("wb resource mount")
+
+## Set cache path to controlled resource bucket
+install.packages("here")
+cat('RENV_PATHS_CACHE="~/workspace/renv-cache/"', file=here::here(".Renviron"), sep="\n")
+
+## Uninstalled limma package will be detected by renv and automatically installed
+try(limma::voom())
+
+## Install and initialize renv
+# Install renv
+install.packages("renv")
+# Init renv with access to bioconductor repos (this will restart RStudio)
+renv::init(bioconductor = TRUE)
+# Check path of renv cache is pointing to ~/workspace/renv-cache/
+renv::paths$cache()
+# Check for inconsistencies between lockfile and dependencies
+renv::status()
+
+## Deactivate renv and re-initialize using the new cache (this will restart RStudio)
+renv::deactivate(clean=TRUE)
+renv::init(bioconductor = TRUE)
+renv::paths$cache()
+renv::status()
+
